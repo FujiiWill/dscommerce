fce7c936010b2b23a25737c37f2c43136e522dce
FujiiWill <fujiiwiliam@gmail.com>
2023-12-27 13:15:30 -0300

    Implementação Order, OrderStatus e User

---
+++ src/main/java/com/fujiiwill/dscommerce/entities/Order.java
@@ -0,0 +1,21 @@
+package com.fujiiwill.dscommerce.entities;
+
+import jakarta.persistence.*;
+
+import java.time.Instant;
+
+@Entity
+@Table(name = "tb-order")
+public class Order {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(columnDefinition = "TIMESTAMP WITHOUT TIME ZONE")
+    private Instant moment;
+    private OrderStatus status;
+
+    @ManyToOne
+    @JoinColumn(name = "client_id")
+    private User client;
+}
