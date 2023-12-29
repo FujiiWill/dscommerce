157985b8bb9331e8a35b13167e04041fb4fcdd3c
FujiiWill <fujiiwiliam@gmail.com>
2023-12-29 10:46:10 -0300

    OrederItem v2

--- src/main/java/com/fujiiwill/dscommerce/entities/Order.java
+++ src/main/java/com/fujiiwill/dscommerce/entities/Order.java
@@ -3,6 +3,9 @@ package com.fujiiwill.dscommerce.entities;
 import jakarta.persistence.*;
 
 import java.time.Instant;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
 
 @Entity
 @Table(name = "tb-order")
@@ -22,8 +25,10 @@ public class Order {
     @OneToOne(mappedBy = "order", cascade = CascadeType.ALL)
     private Payment payment;
 
-    public Order(){
+    @OneToMany(mappedBy = "id.order")
+    private Set<OrderItem> items = new HashSet<>();
 
+    public Order(){
     }
 
     public Order(Long id, Instant moment, OrderStatus status, User client, Payment payment) {
@@ -72,5 +77,12 @@ public class Order {
 
     public void setPayment(Payment payment) {
         this.payment = payment;
+    }
+
+    public Set<OrderItem> getItems() {
+        return items;
+    }
+    public List<Product> getProducts() {
+        return items.stream().map(x -> x.getProduct()).toList();
     }
 }
