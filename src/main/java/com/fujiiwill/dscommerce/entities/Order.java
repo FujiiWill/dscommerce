8217e90f7523fef7ff9979defabfecafb80538ac
FujiiWill <fujiiwiliam@gmail.com>
2023-12-29 11:45:39 -0300

    Projeto corrigido e com seed

--- src/main/java/com/fujiiwill/dscommerce/entities/Order.java
+++ src/main/java/com/fujiiwill/dscommerce/entities/Order.java
@@ -8,7 +8,7 @@ import java.util.List;
 import java.util.Set;
 
 @Entity
-@Table(name = "tb-order")
+@Table(name = "tb_order")
 public class Order {
 
     @Id
--- src/main/java/com/fujiiwill/dscommerce/entities/Product.java
+++ src/main/java/com/fujiiwill/dscommerce/entities/Product.java
@@ -8,7 +8,7 @@ import java.util.List;
 import java.util.Set;
 
 @Entity
-@Table(name = "tb-product")
+@Table(name = "tb_product")
 public class Product {
 
     @Id
@@ -18,7 +18,7 @@ public class Product {
     @Column(columnDefinition = "TEXT")
     private String description;
     private Double price;
-    private String imageUrl;
+    private String imgUrl;
 
     @ManyToMany
     @JoinTable(name = "tb_product_category",
@@ -33,12 +33,12 @@ public class Product {
 
     }
 
-    public Product(Long id, String name, String description, Double price, String imageUrl) {
+    public Product(Long id, String name, String description, Double price, String imgUrl) {
         this.id = id;
         this.name = name;
         this.description = description;
         this.price = price;
-        this.imageUrl = imageUrl;
+        this.imgUrl = imgUrl;
     }
 
     public Long getId() {
@@ -73,12 +73,12 @@ public class Product {
         this.price = price;
     }
 
-    public String getImageUrl() {
-        return imageUrl;
+    public String getImgUrl() {
+        return imgUrl;
     }
 
-    public void setImageUrl(String imageUrl) {
-        this.imageUrl = imageUrl;
+    public void setImgrl(String imageUrl) {
+        this.imgUrl = imgUrl;
     }
 
     public Set<Category> getCategories() {
