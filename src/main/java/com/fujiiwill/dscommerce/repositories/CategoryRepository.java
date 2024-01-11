7a502e127d201cfba125319bbeae53c35be146f1
FujiiWill <fujiiwiliam@gmail.com>
2024-01-11 13:18:00 -0300

    Categories

---
+++ src/main/java/com/fujiiwill/dscommerce/repositories/CategoryRepository.java
@@ -0,0 +1,9 @@
+package com.fujiiwill.dscommerce.repositories;
+
+import com.fujiiwill.dscommerce.entities.Category;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CategoryRepository extends JpaRepository<Category, Long> {
+
+
+}
